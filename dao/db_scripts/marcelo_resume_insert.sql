7affec4087baec12ce4744286d44356b24930a5d
Marcelo Collyer <marcelocollyer@gmail.com>
2018-06-19 10:55:18 -0300

    Adding more data for Marcelo's resume sql insert script

---
+++ dao/db_scripts/marcelo_resume_insert.sql
@@ -0,0 +1,223 @@
+
+{
+	"name": "Marcelo Collyer",
+	"carrer_desc": "+11 years Senior Full Stack Developer always aiming to learn new technologies to provide clients greatest solutions.",
+	"roles_desc": "Software Architect, Technical Leader, Senior Full Stack Developer",
+	"profile_img_url": "https://lh3.googleusercontent.com/airTSbTiNdKtfsw_T62NZoBAuEmMjD21WOdatNKHOOqX-epViWCd2ucXxwrlCQuL3_NtZCWl-zwhLw=s444-rw-no",
+	"email": "marcelocollyer@gmail.com",
+	"phone": "+55 85 98788-0949",
+	"website": "",
+	"linkedin": "linkedin.com/in/marcelocollyer",
+	"github": "github.com/marcelocollyer",
+	"twitter": "twitter.com/marcelocollyer",
+	"experiences": [{
+			"title": "SafeGuard World International",
+			"subtitle": "Software Architect | Technical Leader | Senior Full Stack Developer",
+			"description": "<b>Architect:</b> Provided a enterprise software architecture solution to satisfy client’s needs. Several documents have been created and reviewed periodically to be used as references for cross-cutting architectural aspects. </p><b>Technical Leader</b>: Responsible for leading Developer and QA teams spread across United States, Brazil, England and Sri Lanka. Facilitate a very fluid communication on the project was key to achieve a good balance on a cutting-edge software development with tied deadlines. </p><b>Full Stack Developer:</b> Implemented foundation for all microservices. Wrote most of the more complex features required by the client. Fixed a considerable amount of tickets to unblock implementation/testing of new features made by other developers.",
+			"location": "Brazil",
+			"start_date": "2017-09-02T00:00:00Z"
+		},
+		{
+			"title": "Pearson",
+			"subtitle": "Full Stack Developer",
+			"description": "Implemented an entire ETL application to simplify and speed up legacy processes.",
+			"location": "Brazil",
+			"start_date": "2017-05-02T00:00:00Z",
+			"end_date": "2017-08-02T00:00:00Z"
+		},
+		{
+			"title": "College Nation Sports",
+			"subtitle": "Full Stack Developer",
+			"description": "Contributed implementing a variety of features for a multi-platform mobile app.",
+			"location": "Brazil",
+			"start_date": "2016-12-02T00:00:00Z",
+			"end_date": "2017-03-02T00:00:00Z"
+		},
+		{
+			"title": "Procial",
+			"subtitle": "Full Stack Developer",
+			"description": "Wrote most of main features of the system.",
+			"location": "Brazil",
+			"start_date": "2016-04-02T00:00:00Z",
+			"end_date": "2017-11-02T00:00:00Z"
+		},
+		{
+			"title": "Pearson",
+			"subtitle": "Technical Leader | Full Stack Developer",
+			"description": "Responsible for leading a worldwide spread team on the development of a web enterprise application at San Antonio, Texas. Great relationship with the clients and accomplishment of short deadlines in a fast-paced environment were crucial to the success of this project.",
+			"location": "San Antonio, Texas",
+			"start_date": "2012-01-02T00:00:00Z",
+			"end_date": "2016-03-02T00:00:00Z"
+		},
+		{
+			"title": "Instituto Atlântico",
+			"subtitle": "Full Stack Developer",
+			"description": "<p>Hewlett-Packard:​ Responsible for getting requirements from HP in Palo Alto, CA and guide development team to accomplishment of project goals. Development of successful integrated system using J2EE/C++. <p>Hewlett-Packard:​ Responsible for developing embedded mobile apps for Windows CE 6 using .NET Compact Framework and SQL Server Mobile Edition as well as migrating this software to Win32 platform. <p>Hewlett-Packard:​ Responsible for developing J2EE/Flex distributed solution taking advantage of the beginning of RFID technology. Technologies involved: JSF, Richfaces, PrimeFaces, Ice Faces, HTML5, jquery, Spring, Hibernate, Maven and Ant. <p>Sony Ericsson:​ Development of mobile chat app. J2ME MVC framework and communication middlewares were developed for Jabber, AIM and MSN protocols. <p>HSBC:​ Responsible for A&D and implementation of various financial products based on J2EE platform and Calypso framework. Technologies like Rational Software Modeler, MKS, IBM Quality Center, IBM Macro Runner and Sybase DB were used during this project",
+			"location": "Brazil | United States",
+			"start_date": "2007-09-02T00:00:00Z",
+			"end_date": "2011-12-02T00:00:00Z"
+		},
+		{
+			"title": "Softsite Mobile",
+			"subtitle": "Full Stack Developer",
+			"description": "Development of sales force systems. Web and desktop systems were developed and integrated by using JAVA web-services. Main technologies applied: Google Web toolkit, Google Maps API, Spring, Hibernate, X-fire (CXF), Quartz, Ibatis, Jasper and SQL Server",
+			"location": "Brazil",
+			"start_date": "2006-09-02T00:00:00Z",
+			"end_date": "2011-12-02T00:00:00Z"
+		}
+	],
+	"educations": [{
+		"title": "BSc",
+		"course": "Information Analysis",
+		"location": "Estácio de Sá University, Brazil",
+        "start_date": "2004-01-02T00:00:00Z",
+		"end_date": "2008-01-02T00:00:00Z"
+		
+	}],
+	"languages": [{
+		"name": "English",
+		"level": "Full professional proficiency"
+	},
+    {
+		"name": "Portuguese",
+		"level": "Native"
+	}],
+	"interests": null,
+	"skills": [{
+		"description": "Javascript",
+		"level": "90%",
+		"subset": [
+			{
+			"description": "NodeJS",
+			"level": "70%"
+			},
+			{
+			"description": "ES6",
+			"level": "70%"
+			},
+			{
+			"description": "Typescript",
+			"level": "70%"
+			},
+			{
+			"description": "JQuery",
+			"level": "80%"
+			},
+			{
+			"description": "Ionic",
+			"level": "85%"
+			},
+			{
+			"description": "AngularJS",
+			"level": "90%"
+			},
+			{
+			"description": "Bootstrap",
+			"level": "80%"
+			},
+			{
+			"description": "Angular Material",
+			"level": "90%"
+			},
+			{
+			"description": "PrimeNG",
+			"level": "85%"
+			}			
+		]
+	},
+	{
+		"description": "Java",
+		"level": "95%",
+		"subset": [
+			{
+			"description": "J2EE",
+			"level": "90%"
+			},
+			{
+			"description": "Spring-boot",
+			"level": "90%"
+			},
+			{
+			"description": "Spring Security",
+			"level": "70%"
+			},
+			{
+			"description": "Spring Cloud Netflix",
+			"level": "60%"
+			},
+			{
+			"description": "JPA",
+			"level": "90%"
+			},
+			{
+			"description": "Hibernate Envers",
+			"level": "70%"
+			},
+			{
+			"description": "Flyway",
+			"level": "80%"
+			},
+			{
+			"description": "Liquibase",
+			"level": "80%"
+			},
+			{
+			"description": "JMS",
+			"level": "90%"
+			},
+			{
+			"description": "Eh-cache",
+			"level": "60%"
+			},
+			{
+			"description": "Gemfire",
+			"level": "70%"
+			},
+			{
+			"description": "Maven",
+			"level": "90%"
+			},
+			{
+			"description": "Junit / Mockito",
+			"level": "85%"
+			},
+			{
+			"description": "SonarQube",
+			"level": "85%"
+			},
+			{
+			"description": "Drools Business Engine",
+			"level": "85%"
+			},
+			{
+			"description": "Jasper Reports",
+			"level": "80%"
+			}
+		]
+	},
+	{
+		"description": "Golang",
+		"level": "20%"
+	},
+	{
+		"description": "OSS Netflix",
+		"level": "60%"
+	},
+	{
+		"description": "Amazon Cloud",
+		"level": "70%"
+	},
+	{
+		"description": ".NET",
+		"level": "70%"
+	},
+	{
+		"description": "SQL & NoSQL",
+		"level": "80%"
+	},
+	{
+		"description": "Devops",
+		"level": "40%"
+	}
+]
+}
